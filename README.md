53483ba788d2e514d4bf5479547812ac9a3db46c
huangjunlin <stone58@qq.com>
2017-06-26 15:34:14 +0800

    0.2.11

--- README.md
+++ README.md
@@ -1,83 +1,22 @@
-#WebJet使用说明
-基于websocket的web消息推送组件.
+#XLoger (in construction)
 
-##1. 授权握手
-websocket使用的是持久连接, 系统的设备描述符有限, 所以我们不允许恶意的网络连接占用系统资源.
+A cross-server web-GUI log monitor for cross-language(PHP/Node/Python and more) websever logs.
+			
+[查看中文文档&raquo;](README_CN.MD)			
 
-**1.1 回调URL**
-默认为 /webjet/auth    
-也就是要在同域名下创建接口, 如     
-http://manager.diandao.org/webjet/auth
-Webjet握手时, webjet服务会调用这个接口
+This is a server side program, for clients please use the follows:
+* [xloger-php](https://github.com/idollo/xloger-php)
+* [xloger-python](https://github.com/idollo/xloger-python)
+* [xloger-node](https://gitbug.com/idollo/xloger-node)
 
-**1.2 返回授权结果**
-```php
-#! WebjetController.class.php
-use \Diandao\Webjet;
+## How does XLoger works with web servers?
+![](server/public/imgs/sequence.png)
 
-class WebjetController extends Controller {
-    public function auth(){
-        // 如果用户已登录, 授受连接
-        // 如果是公网游客, 直接auth_accept() (游客在打开官网时, 客户端有权限且只能同时存在一个webjet连接)
-        if($this->is_logined()){
-            Webjet::auth_accept();
-        }else{
-            // 拒绝授权
-            Webjet::auth_reject();
-        }
-    }
-}
+##Requirement
+Before install xloger, you have to install the javascript runtime [Nodejs](https://nodejs.org) first.
+
+##Installation
+Install via npm which is a build-in package managerment for Nodejs.
+```bash
+$ npm install xloger
 ```
-**Webjet::auth_accept($data="", $channel=null)**
-**$data**: 返回握手成功数据, 前端通过"connected"事件获取.
-**$channel**: 用户端加入指定频道, 默认为HTTP_HOST,如: manager.diandao.org 
-
---------------
-##2. 推送消息
-**Webjet::emit($event, $data="", $blocking=false)**
-向当前web会话中的用户推送事件消息, 默认为非堵塞模式, 非堵塞时没有返回值. 而在堵塞模式($blocking=true)时, 发送成功返回True, 发送失败返回False
-
-**Webjet::publish($event, $data="", $channel=null, $blocking=false)**
-广播事件消息给所有已连接的用户. 参数说明同 emit();
-**$channel**: 频道, 每个域名默认都有一个独立的频道/房间, 默认值为当前HTTP_HOST
-
-**绑定会话参数**
-同域名项目下, 会话是同步的, 但有时候会跳转至第三方页面, 如支付页, 支付系统是无法还原之前会话的, 所以跳出本域名前, 记得拼接webjet会话参数, 如: 
-```php
-use \Diandao\Webjet;
-// ...
-$pay_url = "http://diandao.org/pay?oid=xxxx&jetid=".Webjet::jetid();
-header("Location: $url");
-```
-跳出支付页的逻辑:
-```php
-#! pay.php
-require "/phplibs/autoload.php"; # Diandao 模块autoload
-use \Diandao\Webjet;
-
-$jetid = $_REQUEST['fetid'];
-Webjet::jetid($jetid); # 设置webjet会话
-# 给目标用户发送消息
-Webjet::emit("redirect", "http://diandao.org/pay/success?msg=支付成功";
-```
------------
-##3. 监听消息事件
-每个需要的域名项目将挂载 /webjet/ws 连接至web jet服务, 所以/webjet/ws将作为web端的web socket连接接口.
-```html
-<script src="//ddimg.net/js/socket.io.js"></script>
-<script>
-  var socket = io.connect('http://'+location.host, {path:"/webjet/ws/socket.io"});
-  socket.on('connected', function(data){
-      // 握手成功后会得到 Webjet::auth_accept($data)的data数据
-  });
-  // redirect 事件
-  socket.on('redirect', function(url){
-      location.href = url;
-  });
-  socket.on('disconnect', function(){});
-</script>
-```
-
-
-
-
---
+++ server/routes/socket.js
@@ -0,0 +1,158 @@
+"use strict";
+const readline = require('readline');
+const Stream = require('stream');
+
+var filterMgr = require('../lib/filtermgr');
+var uuid = require('node-uuid');
+
+var webPublish = require('../routes/websocket').webPublish;
+
+class SocketReader extends Stream.Readable {
+	_read(size) {
+    	this.push('')
+  	}
+}
+
+
+function writeSocket(socket, data, callback){
+	socket.write( JSON.stringify(data)+"\n", "utf-8", callback );
+}
+
+
+function threadCheckin(socket, data){
+	var accepted = filterMgr.detect(data);
+	serverRegister(socket.remoteAddress, data.host);
+	writeSocket(socket, {accepted: accepted, ip:socket.remoteAddress});
+}
+
+
+function serverRegister(ip, host){
+	var reportors = ncache.get("reportors")||{};
+
+    var server = reportors[ip];
+    if(!server){
+        server = reportors[ip] = {
+            serverip: ip,
+            hosts:[]
+        };
+    }
+    // 保存服务器所绑定的host
+    if(host && host.toLowerCase()!="unknown" && server.hosts.indexOf(host)<0 ){
+        server.hosts.push(host)
+    }
+    ncache.set("reportors", reportors);
+}
+
+
+
+// 消息调度
+function messageDispatcher(socket, message){
+	if(!message) return;
+	var action = message.action
+	,	data = message.data || {};
+	if(!action) return;
+	data.serverIP = socket.remoteAddress;
+	switch(action.toLowerCase()){
+		case "checkin":
+			return threadCheckin(socket, data);
+		case "register":
+			return serverRegister(socket, data);
+		case "trace":
+			return webPublish("log", data);
+		default:
+			return;
+	}
+}
+
+// 缓存sockets连接
+var socketQueue = [];
+var sockets = {};
+
+
+function unisockid(){
+	var sockid = uuid.v4();
+	if(sockets[sockid]) return unisockid();
+	return sockid;
+}
+
+// 缓存socket
+function cachesocket(socket, sockid){
+	if(socketQueue.length > 10000){ // 最大缓存1万个socket
+		var rmid = socketQueue.shift();
+		delete sockets[rmid];
+	}
+	sockets[sockid] = socket;
+	socketQueue.push(sockid);
+}
+
+// 移除socket缓存
+function rmsocket(sockid){
+	var index = socketQueue.indexOf(sockid);
+	if(index>=0){
+		socketQueue.splice(index, 1);
+	}
+	delete sockets[sockid];
+}
+
+// 订阅socket通道
+function subscribe(){
+	// 创建一个TCP服务器实例，调用listen函数开始监听指定端口
+	// 传入net.createServer()的回调函数将作为”connection“事件的处理函数
+	// 在每一个“connection”事件中，该回调函数接收到的socket对象是唯一的
+	var net = require("net");
+	var bind = global.config.bind, host="0.0.0.0", port;
+	switch(({}).toString.apply(bind)){
+		case "[object Array]":
+			port = bind[0];
+			host=bind[1] || host;
+			break;
+		case "[object String]":
+			var m = /(.*?)\:(\d+)$/.exec(bind);
+			if(m){
+				host = m[1]; port=m[2]; break;
+			}
+		default:
+			port = bind;
+	}
+
+	net.createServer(function(socket) {
+	    var stream = new SocketReader;
+	    // socket id 标记
+	    var sockid = unisockid();
+	    // 缓存socket
+	    cachesocket(socket, sockid);
+	    
+	    stream.on("error", function(err){
+	    	console.log(err)
+	    });
+	    var rl = readline.createInterface({ input: stream });
+	    rl.on("line", function(buffer){
+	    	var message = null;
+	    	try{
+				message = JSON.parse(buffer.toString());
+			}catch(e){
+				console.log(e);
+			}
+			messageDispatcher(socket, message);
+	    });
+	    // 为这个socket实例添加一个"data"事件处理函数
+	    socket.on('data', function(buffer) {
+	    	stream.push(buffer);
+	    });
+	    // 为这个socket实例添加一个"close"事件处理函数
+	    socket.on('close', function(data) {
+	    	rmsocket(sockid);
+	    });
+
+	}).listen(port, host);
+	console.log('XLoger Socket listening at %s:%s', host, port);
+}
+
+
+module.exports.subscribe = subscribe;
+module.exports.socketMessage = function(sockid, msgstr){
+	var socket = sockets[sockid];
+	if(socket){
+		sockets[sockid].write(msgstr);
+	}
+};
